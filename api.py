f6cfdc634f4d4d4ac832be64af4831f0c581e6bd
steph-drb <neboolisad8@gmail.com>
2025-04-17 21:50:02 +0200

    Update api.py

--- api.py
+++ api.py
@@ -2,7 +2,7 @@
 from flask import Flask, jsonify, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -100,5 +100,9 @@ def get_top_scores():
     results = ','.join(f"{user.pseudo}:{user.scores[0].score}" for user in top_scores)
     return results, 200
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render donnera un port via la variable PORT
+    app.run(debug=True, host='0.0.0.0', port=port)
+
